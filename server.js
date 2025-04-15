75a32977b18b3631d6969b482851dacd082ff10d
dev-less <131857994+dev-less@users.noreply.github.com>
2025-04-15 02:49:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -55,3 +55,6 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+app.get('/', (req, res) => {
+  res.redirect('/login.html'); 
+});
